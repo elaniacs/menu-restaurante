70757ca77640355d5fa1e9b9a6bbab6feb194bab
elaniacs <carensousaesilva@gmail.com>
2023-04-17 12:31:43 -0300

    Initial Commit

---
+++ LittleLemonMenu/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LittleLemonMenu
+//
+//  Created by Cáren Sousa on 17/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LittleLemonMenu/LittleLemonMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonMenuApp.swift
+//  LittleLemonMenu
+//
+//  Created by Cáren Sousa on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
