8c744d67d50939efa715a81698ea0a436b486e75
elaniacs <carensousaesilva@gmail.com>
2023-05-10 15:46:06 -0300

    Ajuste logo

---
+++ LittleLemonMenu/LittleLemonLogo.swift
@@ -0,0 +1,21 @@
+//
+//  LittleLemonLogo.swift
+//  LittleLemonMenu
+//
+//  Created by Cáren Sousa on 10/05/23.
+//
+
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        Image("littleLemon")
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
+
